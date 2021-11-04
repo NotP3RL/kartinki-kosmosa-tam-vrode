d6677a983d5f6a87799916ebd4e76f576087b88a
NotP3RL <84793664+NotP3RL@users.noreply.github.com>
2021-11-04 15:41:14 +0300

    download pictures from NASA

---
+++ main.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+import os
+from urllib.parse import urlparse
+
+from dotenv import load_dotenv
+import requests
+
+
+def picture_downloader(url, path):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(path, 'wb') as file:
+        file.write(response.content)
+
+
+def fetch_spacex_last_launch():
+    response = requests.get('https://api.spacexdata.com/v4/launches/latest')
+    response.raise_for_status()
+    pictures = response.json()['links']['flickr_images']
+    for number, picture in enumerate(pictures):
+        picture_downloader(picture, f'images/spacex{number}.jpg')
+
+
+def picture_extension(url):
+    parsed_url = urlparse(url)
+    filepath, filename = os.path.split(parsed_url.path)
+    return filename.split(".", 2)[1]
+
+
+def nasa_picture_downloader(count, token):
+    nasa_params = {
+        'count': count,
+        'api_key': token
+    }
+    response = requests.get('https://api.nasa.gov/planetary/apod', params=nasa_params)
+    response.raise_for_status()
+    pictures = response.json()
+    for number, picture in enumerate(pictures):
+        picture_downloader(picture['hdurl'], f'images/nasa_picture{number}.{picture_extension(picture["hdurl"])}')
+
+
+def epic_picture_downloader(token):
+    response = requests.get(f'https://api.nasa.gov/EPIC/api/natural/images?api_key={token}')
+    response.raise_for_status
+    pictures = response.json()[:10]
+    for number, picture in enumerate(pictures):
+        picture_date = datetime.strptime(picture['date'], '%Y-%m-%d %H:%M:%S')
+        picture_downloader(f'https://api.nasa.gov/EPIC/archive/natural/{picture_date.year}/{picture_date.month}/{picture_date.day}/png/{picture["image"]}.png?api_key={token}', f'images/epic{number}.png')
+
+if __name__ == '__main__':
+    load_dotenv()
+    nasa_token = os.getenv("NASA_TOKEN")
+    print(nasa_token)
