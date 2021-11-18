f5e45145b270a533076dc060a7c37da315b45020
NotP3RL <ivan.novikov301208@gmail.com>
2021-11-18 15:33:57 +0300

    added while True cycle for telegram posting with delay

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from urllib.parse import urlparse
 from pathlib import Path
 import random
+import time
 
 from dotenv import load_dotenv
 import requests
@@ -55,14 +56,20 @@ def epic_picture_downloader(token):
 
 if __name__ == '__main__':
     load_dotenv()
-    Path("images/spacex").mkdir(parents=True, exist_ok=True)
-    Path("images/nasa").mkdir(parents=True, exist_ok=True)
-    Path("images/epic").mkdir(parents=True, exist_ok=True)
-    nasa_token = os.getenv("NASA_TOKEN")
-    telegram_token = os.getenv("TELEGRAM_TOKEN")
-    telegram_chat_id = os.getenv("TELEGRAM_CHAT_ID")
-    bot = telegram.Bot(token=telegram_token)
-
-    random_folder_path = f'images/{random.choice(os.listdir("images"))}'
-    random_picture = random.choice(os.listdir(random_folder_path))
-    bot.send_photo(telegram_chat_id, photo=open(f'{random_folder_path}/{random_picture}', 'rb'))
+    Path('images/spacex').mkdir(parents=True, exist_ok=True)
+    Path('images/nasa').mkdir(parents=True, exist_ok=True)
+    Path('images/epic').mkdir(parents=True, exist_ok=True)
+    nasa_token = os.getenv('NASA_TOKEN')
+    telegram_token = os.getenv('TELEGRAM_TOKEN')
+    telegram_chat_id = os.getenv('TELEGRAM_CHAT_ID')
+    telegram_posting_delay = os.getenv('TELEGRAM_POSTING_DELAY')
+    telegram_bot = telegram.Bot(token=telegram_token)
+    while True:
+        fetch_spacex_last_launch()
+        nasa_picture_downloader(5, nasa_token)
+        epic_picture_downloader(nasa_token)
+        random_folder_path = f'images/{random.choice(os.listdir("images"))}'
+        random_picture = random.choice(os.listdir(random_folder_path))
+        telegram_bot.send_photo(telegram_chat_id, photo=open(f'{random_folder_path}/{random_picture}', 'rb'))
+        time.sleep(telegram_posting_delay)
+        
