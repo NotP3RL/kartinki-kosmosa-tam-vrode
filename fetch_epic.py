b65153462ce2dc986b6817c3c56c349c68cddeaf
NotP3RL <ivan.novikov301208@gmail.com>
2021-11-30 16:13:10 +0300

    added safety params in func fetch_epic_pictures

--- fetch_epic.py
+++ fetch_epic.py
@@ -6,12 +6,12 @@ from tools import download_picture
 
 
 def fetch_epic_pictures(count, token):
-    response = requests.get(f'https://api.nasa.gov/EPIC/api/natural/images?api_key={token}')
-    response.raise_for_status
-    pictures = response.json()[:count]
     epic_params = {
         'api_key': token
     }
+    response = requests.get('https://api.nasa.gov/EPIC/api/natural/images', params=epic_params)
+    response.raise_for_status
+    pictures = response.json()[:count]
     for number, picture in enumerate(pictures):
         picture_date = datetime.strptime(picture['date'], '%Y-%m-%d %H:%M:%S')
         formated_picture_date = picture_date.strftime('%Y/%m/%d')
