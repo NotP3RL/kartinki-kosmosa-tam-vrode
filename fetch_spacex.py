880d6ee2ab89fe8ef0a4abd4020e8d8881025fe6
NotP3RL <ivan.novikov301208@gmail.com>
2021-12-07 16:07:57 +0300

    moved request from func get_spacex_random_launch_picture_urls to func fetch_spacex_pictures

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -5,14 +5,14 @@ from tools import download_picture
 
 
 def fetch_spacex_pictures():
-    for number, picture_url in enumerate(get_spacex_random_launch_picture_urls()):
+    response = requests.get('https://api.spacexdata.com/v4/launches')
+    response.raise_for_status()
+    for number, picture_url in enumerate(get_spacex_random_launch_picture_urls(response.json())):
         download_picture(picture_url, f'images/spacex/spacex{number}.jpg')
 
 
-def get_spacex_random_launch_picture_urls():
-    response = requests.get('https://api.spacexdata.com/v4/launches')
-    response.raise_for_status()
-    random_launch = random.choice(response.json())
-    if random_launch['links']['flickr']['original'] != []:
+def get_spacex_random_launch_picture_urls(launches):
+    random_launch = random.choice(launches)
+    if random_launch['links']['flickr']['original']:
         return random_launch['links']['flickr']['original']
     return get_spacex_random_launch_picture_urls()
